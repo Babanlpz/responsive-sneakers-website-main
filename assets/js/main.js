1322e075973f9afb034b84aabd1b214a5dd92933
Esteban <baab2an@live.fr>
2024-07-06 10:41:31 +0200

    FEAT css nav and menu + js show menu and close

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,24 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/* Menu show */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* Menu hidden */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== SWIPER SNEAKERS ===============*/
-
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
